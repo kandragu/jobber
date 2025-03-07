9d0c8d83f5dac0e36961d326ad787096ee6b40e4
Rahu <rahunandan@gmail.com>
2025-03-07 23:21:45 +0800

    runing the service

--- apps/jobber-auth/src/app/app.service.ts
+++ apps/jobber-auth/src/app/app.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
+// import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
 export class AppService {
   constructor(private readonly prismaSerice: PrismaService) {}
 
-  getData(): { message: string } {
-    // this.prismaSerice.user.count();
-    return { message: 'Hello API' };
+  async getData(): Promise<{ message: string; count: number }> {
+    const count = await this.prismaSerice.user.count();
+    return { message: 'Hello API', count };
   }
 }
--- apps/jobber-auth/src/prisma/prisma.service.ts
+++ apps/jobber-auth/src/prisma/prisma.service.ts
@@ -1,5 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma-clients/auth';
 
 @Injectable()
-export class PrismaService extends PrismaClient {}
+export class PrismaService extends PrismaClient implements OnModuleInit {
+  async onModuleInit() {
+    await this.$connect();
+  }
+}
