b1fc5341682cbad9d78e10907423d0f4650c0d66
Rahu <rahunandan@gmail.com>
2025-03-08 16:23:23 +0800

    user graphql createUser mutation

--- apps/jobber-auth/src/app/users/users.resolver.ts
+++ apps/jobber-auth/src/app/users/users.resolver.ts
@@ -1,13 +1,21 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { User } from './models/user.models';
 import { UsersService } from './users.service';
+import { CreateUserInput } from './dto/create-user.input';
+// import { UseGuards } from '@nestjs/common';
 
 @Resolver(() => User)
 export class UsersResolver {
   constructor(private readonly usersService: UsersService) {}
 
+  @Mutation(() => User)
+  async createUser(@Args('createUserInput') createUserInput: CreateUserInput) {
+    return this.usersService.createUser(createUserInput);
+  }
+
+  //   @UseGuards(GqlAuthGuard)
   @Query(() => [User], { name: 'users' })
   async getUsers() {
-    //todo
+    return this.usersService.getUsers();
   }
 }
